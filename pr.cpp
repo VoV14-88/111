ce68787330e46ea787ed918b10945e3404e9bd6d
VoV14-88 <kucerovvova36@gmail.com>
2025-04-29 16:26:53 +0300

    2commit

---
+++ pr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void foo1(){
+    //fuctial 1
+}
+
+void foo2(){
+    //fuctial 3
+}
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    foo1();
+    foo2();
+    foo3();
+    foo4();
+    cout<< "все фукции выполнены";
+}
